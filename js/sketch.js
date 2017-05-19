2cd3cd10539c53ce00aa975f269f6fd932e337a8
Michal Szpakowski <szpakk@gmail.com>
2017-05-19 14:59:00 +0200

    Complete project v2 with multiple darkening square colors

--- js/sketch.js
+++ js/sketch.js
@@ -16,8 +16,23 @@ function paintGrid(numTiles) {
 	var gridWidth = $('.grid').width();
 	grid.css({'height': gridWidth+'px'});
 
-	grid.hover(function() {
-		$(this).addClass('highlight');
+	grid.mouseenter(function() {
+		if (!$(this).hasClass('highlight')) {
+			$(this).addClass('highlight');
+			var hue = Math.floor(Math.random() * 360);
+			var sat = Math.floor(Math.random() * 100);
+			var lig = 90;
+			$(this).data('hue', hue);
+			$(this).data('sat', sat);
+			$(this).data('lig', lig);
+			$(this).css('background-color', 'hsl(' + hue + ', ' + sat + '%, ' + lig + '%)');
+		} else if ($(this).data('lig') > 0) {
+			var hue = $(this).data('hue');
+			var sat = $(this).data('sat');
+			var lig = $(this).data('lig') - 10;
+			$(this).data('lig', lig);
+			$(this).css('background-color', 'hsl(' + hue + ', ' + sat + '%, ' + lig + '%)');
+		}
 	});
 }
 
