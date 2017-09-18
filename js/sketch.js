5f9e9c387a522457fba48d5e33689c051785b9a4
Michal Szpakowski <szpakk@gmail.com>
2017-09-18 11:04:22 +0200

    Refactor code

--- js/sketch.js
+++ js/sketch.js
@@ -1,22 +1,7 @@
-var squares = 16;
-
-function paintGrid(numTiles) {
-	$('.container').empty();
-	var container = $('.container');
-	for (var i = 0; i < numTiles; i++) {
-		container.append($('<div class="row"></div>'));
-	}
-	var row = $('.row');
-	for (var i = 0; i < numTiles; i++) {
-		row.append($('<div class="grid"></div>'));
-	}
-	var grid = $('.grid')
-	var contWidth = $('.container').width();
-	grid.css({'width': contWidth/numTiles + 'px'});
-	var gridWidth = $('.grid').width();
-	grid.css({'height': gridWidth+'px'});
-
-	grid.mouseenter(function() {
+SKETCH = {
+	squares: 16,
+	
+	paintTile: function() {
 		if (!$(this).hasClass('highlight')) {
 			$(this).addClass('highlight');
 			var hue = Math.floor(Math.random() * 360);
@@ -33,24 +18,50 @@ function paintGrid(numTiles) {
 			$(this).data('lig', lig);
 			$(this).css('background-color', 'hsl(' + hue + ', ' + sat + '%, ' + lig + '%)');
 		}
-	});
-}
+	},
+  
+  buildGrid: function() {
+		var $container = $('.container');
+		for (var i = 0; i < SKETCH.squares; i++) {
+			$container.append($('<div class="row"></div>'));
+		}
+		var $row = $('.row');
+		for (var i = 0; i < SKETCH.squares; i++) {
+			$row.append($('<div class="grid"></div>'));
+		}
+	},
 
-$(document).ready(function(){
-	paintGrid(squares);
-});
+	paintGrid: function() {
+		$('.container').empty();
+		SKETCH.buildGrid();
+		var $grid = $('.grid')
+		var containerWidth = $('.container').width();
+		$grid.css({'width': containerWidth/SKETCH.squares + 'px'});
+		var gridWidth = $('.grid').width();
+		$grid.css({'height': gridWidth+'px'});
 
-function resetGrid() {
-	var newSquares = prompt("Please how many squares per side you want the grid to be (between 1 and 64):", squares);
-	if (newSquares === null) {
-		return;
-	}
-	while (newSquares < 1 || newGrid > 64) {
-		newSquares = prompt("Wrong square number! Please enter number between 1 and 64:", grid);
+		$grid.mouseenter(SKETCH.paintTile);
+	},
+
+	resetGrid: function() {
+		var newSquares = prompt("Please how many squares per side you want the grid to be (between 1 and 64):", SKETCH.squares);
 		if (newSquares === null) {
 			return;
 		}
+		while (newSquares < 1 || newSquares > 64) {
+			newSquares = prompt("Wrong square number! Please enter a number between 1 and 64:", SKETCH.squares);
+			if (newSquares === null) {
+				return;
+			}
+		}
+		SKETCH.squares = newSquares;
+		SKETCH.paintGrid();
 	}
-	paintGrid(newSquares)
-	squares = newSquares;
-}
+};
+
+$(document).ready(function(){
+	SKETCH.paintGrid();
+	$('#reset').on("click", function() {
+		SKETCH.resetGrid();
+	});
+});
